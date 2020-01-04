99875243890778c1d4a8218cb802852c31e7e8ef
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-04 07:18:53 -0600

    First true test

--- src/test/kotlin/hm/binkley/math/RationalTest.kt
+++ src/test/kotlin/hm/binkley/math/RationalTest.kt
@@ -1,9 +1,15 @@
 package hm.binkley.math
 
+import hm.binkley.math.Rational.Companion.NaN
+import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Test
 
 internal class RationalTest {
     @Test
-    fun nada() {
+    fun `should construct NaN`() {
+        assertSame(
+            NaN,
+            0 over 0
+        )
     }
 }
