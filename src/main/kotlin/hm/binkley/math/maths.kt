395a25d889f95e07cb1933ba79370169c9ae1a3a
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-05-25 08:10:31 -0500

    Cleaner

--- src/main/kotlin/hm/binkley/math/maths.kt
+++ src/main/kotlin/hm/binkley/math/maths.kt
@@ -2,17 +2,20 @@ package hm.binkley.math
 
 import java.math.BigDecimal
 import java.math.BigInteger
+import java.math.BigInteger.ONE
+import java.math.BigInteger.TEN
+import java.math.BigInteger.TWO
+import java.math.BigInteger.ZERO
 
 internal typealias BInt = BigInteger
 internal typealias BDouble = BigDecimal
 
-internal fun BInt.isZero() = this == BInt.ZERO
-internal fun BInt.isOne() = this == BInt.ONE
-internal fun BInt.isTwo() = this == BInt.TWO
-internal fun BInt.isTen() = this == BInt.TEN
+internal fun BInt.isZero() = this == ZERO
+internal fun BInt.isOne() = this == ONE
+internal fun BInt.isTwo() = this == TWO
+internal fun BInt.isTen() = this == TEN
 
-internal fun BInt.isDyadic() =
-    (isOne() || (this % BInt.TWO).isZero())
+internal fun BInt.isDyadic() = (isOne() || (this % TWO).isZero())
 
 internal fun BInt.isPAdic(p: Long) =
     (isOne() || (this % BInt.valueOf(p)).isZero())
@@ -24,4 +27,4 @@ internal fun mantissa(d: Double) = d.toBits() and 0xfffffffffffffL
 
 internal fun BInt.lcm(other: BInt) = (this * (other / gcd(other))).abs()
 
-internal fun BInt.isEven() = BInt.ZERO == this % BInt.TWO
+internal fun BInt.isEven() = ZERO == this % TWO
