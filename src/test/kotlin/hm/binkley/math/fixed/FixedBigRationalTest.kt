bfc495381448672148018878a77509e5b596045b
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-07-15 12:51:06 -0500

    Organization

--- src/test/kotlin/hm/binkley/math/fixed/FixedBigRationalTest.kt
+++ src/test/kotlin/hm/binkley/math/fixed/FixedBigRationalTest.kt
@@ -9,7 +9,6 @@ import hm.binkley.math.ceil
 import hm.binkley.math.compareTo
 import hm.binkley.math.div
 import hm.binkley.math.divideAndRemainder
-import hm.binkley.math.downTo
 import hm.binkley.math.fixed.FixedBigRational.Companion.ONE
 import hm.binkley.math.fixed.FixedBigRational.Companion.TEN
 import hm.binkley.math.fixed.FixedBigRational.Companion.TWO
@@ -26,12 +25,10 @@ import hm.binkley.math.plus
 import hm.binkley.math.rangeTo
 import hm.binkley.math.rem
 import hm.binkley.math.sqrt
-import hm.binkley.math.step
 import hm.binkley.math.times
 import hm.binkley.math.truncate
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
@@ -862,136 +859,6 @@ internal class FixedBigRationalTest {
                 (-128).toByte(),
                 ((Byte.MAX_VALUE + 1) over 1).toByte()
             )
-        }
-    }
-
-    @Nested
-    inner class ProgressionTests {
-        @Suppress("ReplaceCallWithBinaryOperator")
-        @Test
-        fun `should be itself`() {
-            val zeroToOne = ZERO..ONE
-            assertEquals(
-                zeroToOne,
-                ZERO..ONE
-            )
-            assertEquals(zeroToOne, zeroToOne)
-            assertFalse(zeroToOne.equals(ZERO))
-            assertEquals(
-                zeroToOne,
-                zeroToOne step ONE
-            )
-            assertNotEquals(
-                zeroToOne step ONE,
-                zeroToOne step TWO
-            )
-            assertNotEquals(
-                zeroToOne,
-                ZERO..TWO
-            )
-            assertEquals(
-                (zeroToOne step (1 over 2)).hashCode(),
-                (zeroToOne step (1 over 2)).hashCode()
-            )
-        }
-
-        @Test
-        fun `should pretty print`() {
-            assertEquals(
-                "0..1 step 1",
-                "${(ZERO..ONE)}"
-            )
-            assertEquals(
-                "0..1 step 2",
-                "${(ZERO..ONE step 2)}"
-            )
-            assertEquals(
-                "1 downTo 0 step -1",
-                "${(ONE downTo ZERO)}"
-            )
-            assertEquals(
-                "1 downTo 0 step -2",
-                "${(1 downTo ZERO step -TWO)}"
-            )
-        }
-
-        @Test
-        fun `should progress`() {
-            assertTrue(((ZERO..(-ONE)).isEmpty()))
-            assertTrue((ZERO..TWO).contains(ONE))
-            assertEquals(
-                listOf(ONE, (2 over 1)),
-                ((1 over 1)..(5 over 2)).toList()
-            )
-            val three = 3 over 1
-            assertEquals(
-                listOf(ONE, three),
-                (BDouble.ONE..three step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (ONE..BDouble.valueOf(3) step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (1.0..three step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (ONE..3.0 step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (1.0f..three step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (ONE..3.0f step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (BInt.ONE..three step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (ONE..BInt.valueOf(3) step 2).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (1L..three step 2L).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (ONE..3L step 2L).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (1..three step (2 over 1)).toList()
-            )
-            assertEquals(
-                listOf(ONE, three),
-                (ONE..3 step (2 over 1)).toList()
-            )
-            assertEquals(
-                listOf((2 over 1), ONE),
-                ((2 over 1) downTo (1 over 2) step -BInt.ONE).toList()
-            )
-        }
-
-        @Suppress("ControlFlowWithEmptyBody")
-        @Test
-        fun `should not progress`() {
-            val noop = { }
-
-            assertThrows<IllegalStateException> {
-                for (r in ZERO..ONE step -1) noop()
-            }
-            assertThrows<IllegalStateException> {
-                for (r in BInt.ONE downTo ZERO step 1); noop()
-            }
-            assertThrows<IllegalStateException> {
-                for (r in 0L downTo ZERO step ZERO); noop()
-            }
         }
     }
 
