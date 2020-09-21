cd028d32647854e8cae65a703963de22b1a728d0
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-09-21 07:32:04 -0500

    Kotest, part #6

--- src/test/kotlin/hm/binkley/math/fixed/FixedProgressionTests.kt
+++ src/test/kotlin/hm/binkley/math/fixed/FixedProgressionTests.kt
@@ -7,109 +7,68 @@ import hm.binkley.math.fixed.FixedBigRational.Companion.TWO
 import hm.binkley.math.fixed.FixedBigRational.Companion.ZERO
 import hm.binkley.math.rangeTo
 import hm.binkley.math.step
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Assertions.assertNotEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 @Nested
 class FixedProgressionTests {
+    // TODO: What goes wrong here?
+    private val shouldNotBeWorks = false
+
     @Suppress("ReplaceCallWithBinaryOperator")
     @Test
     fun `should be itself`() {
         val zeroToOne = ZERO..ONE
-        assertEquals(zeroToOne, ZERO..ONE)
-        assertEquals(zeroToOne, zeroToOne)
-        assertFalse(zeroToOne.equals(ZERO))
-        assertEquals(zeroToOne, zeroToOne step ONE)
-        assertNotEquals(zeroToOne step ONE, zeroToOne step TWO)
-        assertNotEquals(zeroToOne, ZERO..TWO)
-        assertEquals(
+        (ZERO..ONE).shouldBe(zeroToOne)
+        zeroToOne.equals(ZERO).shouldBeFalse()
+        (zeroToOne step ONE).shouldBe(zeroToOne)
+        if (shouldNotBeWorks)
+            (zeroToOne step TWO).shouldNotBe(zeroToOne step ONE)
+        else
+            assertNotEquals(zeroToOne step ONE, zeroToOne step TWO)
+        if (shouldNotBeWorks)
+            (ZERO..TWO).shouldNotBe(zeroToOne)
+        else
+            assertNotEquals(zeroToOne, ZERO..TWO)
+        (zeroToOne step (1 over 2)).hashCode().shouldBe(
             (zeroToOne step (1 over 2)).hashCode(),
-            (zeroToOne step (1 over 2)).hashCode()
         )
     }
 
     @Test
     fun `should pretty print`() {
-        assertEquals(
-            "0..1 step 1",
-            "${(ZERO..ONE)}"
-        )
-        assertEquals(
-            "0..1 step 2",
-            "${(ZERO..ONE step 2)}"
-        )
-        assertEquals(
-            "1 downTo 0 step -1",
-            "${(ONE downTo ZERO)}"
-        )
-        assertEquals(
-            "1 downTo 0 step -2",
-            "${(1 downTo ZERO step -TWO)}"
-        )
+        "${(ZERO..ONE)}".shouldBe("0..1 step 1")
+        "${(ZERO..ONE step 2)}".shouldBe("0..1 step 2")
+        "${(ONE downTo ZERO)}".shouldBe("1 downTo 0 step -1")
+        "${(1 downTo ZERO step -TWO)}".shouldBe("1 downTo 0 step -2")
     }
 
     @Test
     fun `should progress`() {
-        assertTrue(((ZERO..(-ONE)).isEmpty()))
-        assertTrue((ZERO..TWO).contains(ONE))
-        assertEquals(
-            listOf(ONE, (2 over 1)),
-            ((1 over 1)..(5 over 2)).toList()
-        )
+        (ZERO..(-ONE)).isEmpty().shouldBeTrue()
+        (ZERO..TWO).contains(ONE).shouldBeTrue()
+        ((1 over 1)..(5 over 2)).toList().shouldBe(listOf(ONE, (2 over 1)))
         val three = 3 over 1
-        assertEquals(listOf(ONE, three), (1.0.big..three step 2).toList())
-        assertEquals(
-            listOf(ONE, three),
-            (ONE..3.0.big step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (1.0..three step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (ONE..3.0 step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (1.0f..three step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (ONE..3.0f step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (1.big..three step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (ONE..3.big step 2).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (1L..three step 2L).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (ONE..3L step 2L).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (1..three step (2 over 1)).toList()
-        )
-        assertEquals(
-            listOf(ONE, three),
-            (ONE..3 step (2 over 1)).toList()
-        )
-        assertEquals(
+        (1.0.big..three step 2).toList().shouldBe(listOf(ONE))
+        (ONE..3.0.big step 2).toList().shouldBe(listOf(ONE, three))
+        (1.0..three step 2).toList().shouldBe(listOf(ONE, three))
+        (ONE..3.0 step 2).toList().shouldBe(listOf(ONE, three))
+        (1.0f..three step 2).toList().shouldBe(listOf(ONE, three))
+        (ONE..3.0f step 2).toList().shouldBe(listOf(ONE, three))
+        (1.big..three step 2).toList().shouldBe(listOf(ONE, three))
+        (ONE..3.big step 2).toList().shouldBe(listOf(ONE, three))
+        (1L..three step 2L).toList().shouldBe(listOf(ONE, three))
+        (ONE..3L step 2L).toList().shouldBe(listOf(ONE, three))
+        (1..three step (2 over 1)).toList().shouldBe(listOf(ONE, three))
+        (ONE..3 step (2 over 1)).toList().shouldBe(listOf(ONE, three))
+        ((2 over 1) downTo (1 over 2) step -(1.big)).toList().shouldBe(
             listOf((2 over 1), ONE),
-            ((2 over 1) downTo (1 over 2) step -(1.big)).toList()
         )
     }
 
@@ -118,13 +77,13 @@ class FixedProgressionTests {
     fun `should not progress`() {
         val noop = { }
 
-        assertThrows<IllegalStateException> {
+        shouldThrow<IllegalStateException> {
             for (r in ZERO..ONE step -1) noop()
         }
-        assertThrows<IllegalStateException> {
+        shouldThrow<IllegalStateException> {
             for (r in 1.big downTo ZERO step 1); noop()
         }
-        assertThrows<IllegalStateException> {
+        shouldThrow<IllegalStateException> {
             for (r in 0L downTo ZERO step ZERO); noop()
         }
     }
