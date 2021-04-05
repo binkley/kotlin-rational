7a6af29db2ab62a4edfd37cf4b401bd77d7c6ab8
Brian Oxley <boxley@thoughtworks.com>
2021-04-06 07:05:23 +0800

    Bump Batect

--- batect.cmd
+++ batect.cmd
@@ -6,7 +6,7 @@ rem For more information, visit https://github.com/batect/batect.
 
 setlocal EnableDelayedExpansion
 
-set "version=0.66.1"
+set "version=0.70.0"
 
 if "%BATECT_CACHE_DIR%" == "" (
     set "BATECT_CACHE_DIR=%USERPROFILE%\.batect\cache"
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'^
 
 ^
 
-$Version='0.66.1'^
+$Version='0.70.0'^
 
 ^
 
@@ -42,13 +42,13 @@ function getValueOrDefault($value, $default) {^
 
 ^
 
-$DownloadUrlRoot = getValueOrDefault $env:BATECT_DOWNLOAD_URL_ROOT "https://dl.bintray.com/batect/batect"^
+$DownloadUrlRoot = getValueOrDefault $env:BATECT_DOWNLOAD_URL_ROOT "https://updates.batect.dev/v1/files"^
 
 $UrlEncodedVersion = [Uri]::EscapeDataString($Version)^
 
-$DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/bin/batect-$UrlEncodedVersion.jar"^
+$DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/batect-$UrlEncodedVersion.jar"^
 
-$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '635986dfc452d730afd4d512d55f17f9365cfce08afc6d5194b87c7dda3d75cb'^
+$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '3d649c70079e2c529c0de5aab33fdd27229e3012f9c1de4a01c305fe7fba504a'^
 
 ^
 
