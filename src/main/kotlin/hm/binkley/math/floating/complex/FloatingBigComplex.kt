5d611af70a73347d5d6efa454c18b795549d301b
Brian Oxley <boxley@thoughtworks.com>
2022-02-24 10:52:43 +0800

    Floating complex type for `FloatingBigRational`

---
+++ src/main/kotlin/hm/binkley/math/floating/complex/FloatingBigComplex.kt
@@ -0,0 +1,421 @@
+package hm.binkley.math.floating.complex
+
+import hm.binkley.math.BFixed
+import hm.binkley.math.BFloating
+import hm.binkley.math.algebra.Field
+import hm.binkley.math.algebra.FieldCompanion
+import hm.binkley.math.floating.FloatingBigRational
+import hm.binkley.math.floating.FloatingBigRational.Companion.TWO
+import hm.binkley.math.floating.complex.FloatingBigComplex.Companion.ONE
+import hm.binkley.math.floating.toBigRational
+import hm.binkley.math.isZero
+import hm.binkley.math.sqrt
+import hm.binkley.math.sqrtApproximated
+import java.math.BigDecimal
+import java.math.BigInteger
+import kotlin.math.absoluteValue
+
+public data class FloatingBigComplex(
+    val real: FloatingBigRational,
+    val imag: FloatingBigImaginary,
+) : Field<FloatingBigComplex> {
+    // Not covered in tests -- API ensures 0 and 1 are defined
+    override val companion: Companion get() = FloatingBigComplex
+
+    override operator fun unaryMinus(): FloatingBigComplex = -real + -imag
+
+    /** *NB* &mdash; `1/(a+bi)` is `(a-bi)/(a² + b²)`. */
+    override fun unaryDiv(): FloatingBigComplex = conjugate / det
+
+    override operator fun plus(addend: FloatingBigComplex): FloatingBigComplex =
+        (real + addend.real) + (imag + addend.imag)
+
+    override operator fun times(factor: FloatingBigComplex): FloatingBigComplex =
+        (real * factor.real + imag * factor.imag) +
+                (real * factor.imag + imag * factor.real)
+
+    override operator fun div(divisor: FloatingBigComplex): FloatingBigComplex =
+        this * divisor.unaryDiv()
+
+    override fun toString(): String =
+        if (FloatingBigRational.ZERO > imag.value) "$real-${-imag}" else "$real+$imag"
+
+    public companion object : FieldCompanion<FloatingBigComplex> {
+        override val ZERO: FloatingBigComplex =
+            FloatingBigComplex(
+                FloatingBigRational.ZERO,
+                FloatingBigRational.ZERO.i
+            )
+        override val ONE: FloatingBigComplex =
+            FloatingBigComplex(
+                FloatingBigRational.ONE,
+                FloatingBigRational.ZERO.i
+            )
+    }
+}
+
+// Properties
+
+public val FloatingBigComplex.conjugate: FloatingBigComplex get() = real - imag
+public val FloatingBigComplex.det: FloatingBigRational
+    get() = real * real - imag * imag
+public val FloatingBigComplex.absoluteValue: FloatingBigRational get() = det.sqrt()
+public val FloatingBigComplex.reciprocal: FloatingBigComplex get() = unaryDiv()
+
+// Factories, plus and minus real+imag (include improper order like "i+1")
+
+public operator fun FloatingBigRational.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    FloatingBigComplex(this, imag)
+
+public operator fun FloatingBigImaginary.plus(real: FloatingBigRational): FloatingBigComplex =
+    real + this
+
+public operator fun BFloating.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    toBigRational() + imag
+
+public operator fun FloatingBigImaginary.plus(real: BFloating): FloatingBigComplex =
+    real + this
+
+public operator fun Double.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    toBigRational() + imag
+
+public operator fun FloatingBigImaginary.plus(real: Double): FloatingBigComplex =
+    real + this
+
+public operator fun Float.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    toBigRational() + imag
+
+public operator fun FloatingBigImaginary.plus(real: Float): FloatingBigComplex =
+    real + this
+
+public operator fun BFixed.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    toBigRational() + imag
+
+public operator fun FloatingBigImaginary.plus(real: BFixed): FloatingBigComplex =
+    real + this
+
+public operator fun Long.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    toBigRational() + imag
+
+public operator fun FloatingBigImaginary.plus(real: Long): FloatingBigComplex =
+    real + this
+
+public operator fun Int.plus(imag: FloatingBigImaginary): FloatingBigComplex =
+    toBigRational() + imag
+
+public operator fun FloatingBigImaginary.plus(real: Int): FloatingBigComplex =
+    real + this
+
+public operator fun FloatingBigImaginary.minus(real: FloatingBigRational): FloatingBigComplex =
+    -real + this
+
+public operator fun FloatingBigRational.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+public operator fun FloatingBigImaginary.minus(real: BFloating): FloatingBigComplex =
+    -real + this
+
+public operator fun BFloating.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+public operator fun FloatingBigImaginary.minus(real: Double): FloatingBigComplex =
+    -real + this
+
+public operator fun Double.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+public operator fun FloatingBigImaginary.minus(real: Float): FloatingBigComplex =
+    -real + this
+
+public operator fun Float.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+public operator fun FloatingBigImaginary.minus(real: BFixed): FloatingBigComplex =
+    -real + this
+
+public operator fun BFixed.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+public operator fun FloatingBigImaginary.minus(real: Long): FloatingBigComplex =
+    -real + this
+
+public operator fun Long.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+public operator fun FloatingBigImaginary.minus(real: Int): FloatingBigComplex =
+    -real + this
+
+public operator fun Int.minus(imag: FloatingBigImaginary): FloatingBigComplex =
+    this + -imag
+
+// Addition operator
+
+public operator fun FloatingBigComplex.plus(addend: FloatingBigImaginary): FloatingBigComplex =
+    this + (FloatingBigRational.ZERO + addend)
+
+public operator fun FloatingBigImaginary.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigRational.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: FloatingBigRational): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+public operator fun BFloating.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: BFloating): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+public operator fun Double.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: Double): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+public operator fun Float.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: Float): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+public operator fun BFixed.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: BFixed): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+public operator fun Long.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: Long): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+public operator fun Int.plus(addend: FloatingBigComplex): FloatingBigComplex =
+    addend + this
+
+public operator fun FloatingBigComplex.plus(addend: Int): FloatingBigComplex =
+    this + (addend + FloatingBigRational.ZERO.i)
+
+// Subtraction operator
+
+public operator fun FloatingBigComplex.minus(subtrahend: FloatingBigImaginary): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigImaginary.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigRational.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: FloatingBigRational): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun BFloating.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: BFloating): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun Double.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: Double): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun Float.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: Float): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun BFixed.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: BFixed): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun Long.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: Long): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun Int.minus(subtrahend: FloatingBigComplex): FloatingBigComplex =
+    this + -subtrahend
+
+public operator fun FloatingBigComplex.minus(subtrahend: Int): FloatingBigComplex =
+    this + -subtrahend
+
+// Multiplication operator
+
+public operator fun FloatingBigComplex.times(multiplier: FloatingBigImaginary): FloatingBigComplex =
+    this * (FloatingBigComplex.ZERO + multiplier)
+
+public operator fun FloatingBigImaginary.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigRational.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: FloatingBigRational): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+public operator fun BFloating.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: BFloating): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+public operator fun Double.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: Double): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+public operator fun Float.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: Float): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+public operator fun BFixed.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: BFixed): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+public operator fun Long.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: Long): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+public operator fun Int.times(multiplier: FloatingBigComplex): FloatingBigComplex =
+    multiplier * this
+
+public operator fun FloatingBigComplex.times(multiplier: Int): FloatingBigComplex =
+    this * (multiplier + FloatingBigRational.ZERO.i)
+
+// Division operator
+
+public operator fun FloatingBigComplex.div(divisor: FloatingBigImaginary): FloatingBigComplex =
+    this / (FloatingBigComplex.ZERO + divisor)
+
+public operator fun FloatingBigImaginary.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigRational.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: FloatingBigRational): FloatingBigComplex =
+    real / divisor + (imag.value / divisor).i
+
+public operator fun BFloating.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: BFloating): FloatingBigComplex =
+    this / divisor.toBigRational()
+
+public operator fun Double.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: Double): FloatingBigComplex =
+    this / divisor.toBigRational()
+
+public operator fun Float.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: Float): FloatingBigComplex =
+    this / divisor.toBigRational()
+
+public operator fun BFixed.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: BFixed): FloatingBigComplex =
+    this / divisor.toBigRational()
+
+public operator fun Long.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: Long): FloatingBigComplex =
+    this / divisor.toBigRational()
+
+public operator fun Int.div(divisor: FloatingBigComplex): FloatingBigComplex =
+    divisor / this
+
+public operator fun FloatingBigComplex.div(divisor: Int): FloatingBigComplex =
+    this / divisor.toBigRational()
+
+// Functions
+
+public fun FloatingBigComplex.toImaginary(): FloatingBigImaginary =
+    if (real.isZero()) imag
+    else throw ArithmeticException("Not imaginary: $this")
+
+public fun FloatingBigComplex.toBigRational(): FloatingBigRational =
+    if (imag.isZero()) real
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.toBigDecimal(): BigDecimal =
+    if (imag.isZero()) real.toBigDecimal()
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.toDouble(): Double =
+    if (imag.isZero()) real.toDouble()
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.toFloat(): Float =
+    if (imag.isZero()) real.toFloat()
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.toBigInteger(): BigInteger =
+    if (imag.isZero()) real.toBigInteger()
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.toLong(): Long =
+    if (imag.isZero()) real.toLong()
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.toInt(): Int =
+    if (imag.isZero()) real.toInt()
+    else throw ArithmeticException("Not real: $this")
+
+public fun FloatingBigComplex.modulusApproximated(): FloatingBigRational =
+    det.sqrtApproximated()
+
+public fun FloatingBigComplex.sqrtApproximated(): FloatingBigComplex {
+    val gamma = ((real + modulusApproximated()) / TWO).sqrtApproximated()
+    val delta = imag.value.sign *
+            ((-real + modulusApproximated()) / TWO).sqrtApproximated()
+    return gamma + delta.i
+}
+
+@Suppress("DANGEROUS_CHARACTERS", "FunctionName")
+public infix fun FloatingBigComplex.`^`(n: Int): FloatingBigComplex = pow(n)
+
+/**
+ * Note: Following expectations for discrete exponents, `0^0` is defined as `1`.
+ *
+ * @todo Improve on brute force
+ */
+public fun FloatingBigComplex.pow(n: Int): FloatingBigComplex {
+    when (n) {
+        0 -> return ONE
+        1 -> return this
+        -1 -> return unaryDiv()
+    }
+
+    tailrec fun FloatingBigComplex.pow0(
+        exponent: Int,
+        current: FloatingBigComplex,
+    ): FloatingBigComplex = when (exponent) {
+        0 -> current
+        else -> pow0(exponent - 1, this * current)
+    }
+
+    val z = pow0(n.absoluteValue - 1, this)
+
+    return if (0 > n) z.unaryDiv() else z
+}
---
+++ src/test/kotlin/hm/binkley/math/floating/complex/FloatingBigComplexTest.kt
@@ -0,0 +1,223 @@
+package hm.binkley.math.floating.complex
+
+import hm.binkley.math.BFixed
+import hm.binkley.math.BFloating
+import hm.binkley.math.big
+import hm.binkley.math.floating.FloatingBigRational
+import hm.binkley.math.floating.complex.FloatingBigComplex.Companion.ONE
+import hm.binkley.math.floating.complex.FloatingBigComplex.Companion.ZERO
+import hm.binkley.math.floating.complex.FloatingBigImaginary.Companion.I
+import hm.binkley.math.floating.isNaN
+import hm.binkley.math.floating.over
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+private val ONE_PLUS_I = FloatingBigRational.ONE + I
+private val ONE_MINUS_I = FloatingBigRational.ONE - I
+private val NEG_ONE_MINUS_I = -FloatingBigRational.ONE - I
+
+internal class FloatingBigComplexTest {
+    @Test
+    fun `should have constants`() {
+        ZERO shouldBe 0 + 0.i
+        ONE shouldBe 1 + 0.i
+    }
+
+    @Test
+    fun `should destructure`() {
+        val (real, imag) = ONE_PLUS_I
+        real shouldBe ONE_PLUS_I.real
+        imag shouldBe ONE_PLUS_I.imag
+    }
+
+    @Test
+    fun `should pretty print`() {
+        "$ONE_PLUS_I" shouldBe "1+1i"
+        "$ONE_MINUS_I" shouldBe "1-1i"
+    }
+
+    @Test
+    fun `should conjugate`() {
+        ONE_PLUS_I.conjugate shouldBe ONE_MINUS_I
+    }
+
+    @Test
+    fun `should determinate`() {
+        (2 + 2.i).det shouldBe (8 over 1)
+    }
+
+    @Test
+    fun `should absolve`() {
+        (3 + 4.i).absoluteValue shouldBe (5 over 1)
+    }
+
+    @Test
+    fun `should absolve approximately`() {
+        (8 + 25.i).modulusApproximated() shouldBe
+                (410137648387709L.big over 15625000000000L.big)
+    }
+
+    @Test
+    fun `should reciprocate`() {
+        val z = 2 + 2.i
+        z.unaryDiv() shouldBe ((1 over 4) - (1 over 4).i)
+        z.reciprocal shouldBe z.unaryDiv()
+    }
+
+    @Test
+    fun `should posite`() {
+        +ONE_PLUS_I shouldBe ONE_PLUS_I
+    }
+
+    @Test
+    fun `should negate`() {
+        -ONE_PLUS_I shouldBe NEG_ONE_MINUS_I
+    }
+
+    @Test
+    fun `should add`() {
+        val complex = 1 + I
+        val left = 2 + I // complex + one
+        val right = 2 + I // one + complex
+
+        (complex + complex) shouldBe 2 + 2.i
+        (complex + FloatingBigRational.ONE) shouldBe left
+        (FloatingBigRational.ONE + complex) shouldBe right
+        (complex + 1.0.big) shouldBe left
+        (1.0.big + complex) shouldBe right
+        (complex + 1.0) shouldBe left
+        (1.0 + complex) shouldBe right
+        (complex + 1.0f) shouldBe left
+        (1.0f + complex) shouldBe right
+        (complex + 1.big) shouldBe left
+        (1.big + complex) shouldBe right
+        (complex + 1L) shouldBe left
+        (1L + complex) shouldBe right
+        (complex + 1) shouldBe left
+        (1 + complex) shouldBe right
+    }
+
+    @Test
+    fun `should subtract`() {
+        val complex = 1 + I
+        val left = 0 + I // complex - one
+        val right = 0 + -I // one - complex
+
+        (complex - complex) shouldBe ZERO
+        (complex - FloatingBigRational.ONE) shouldBe left
+        (FloatingBigRational.ONE - complex) shouldBe right
+        (complex - 1.0.big) shouldBe left
+        (1.0.big - complex) shouldBe right
+        (complex - 1.0) shouldBe left
+        (1.0 - complex) shouldBe right
+        (complex - 1.0f) shouldBe left
+        (1.0f - complex) shouldBe right
+        (complex - 1.big) shouldBe left
+        (1.big - complex) shouldBe right
+        (complex - 1L) shouldBe left
+        (1L - complex) shouldBe right
+        (complex - 1) shouldBe left
+        (1 - complex) shouldBe right
+    }
+
+    @Test
+    fun `should multiply`() {
+        val complex = 1 + I
+        val left = 2 + 2.i // complex * two
+        val right = 2 + 2.i // two * complex
+
+        (complex * complex) shouldBe (0 + 2.i)
+        (complex * FloatingBigRational.TWO) shouldBe left
+        (FloatingBigRational.TWO * complex) shouldBe right
+        (complex * 2.0.big) shouldBe left
+        (2.0.big * complex) shouldBe right
+        (complex * 2.0) shouldBe left
+        (2.0 * complex) shouldBe right
+        (complex * 2.0f) shouldBe left
+        (2.0f * complex) shouldBe right
+        (complex * 2.big) shouldBe left
+        (2.big * complex) shouldBe right
+        (complex * 2L) shouldBe left
+        (2L * complex) shouldBe right
+        (complex * 2) shouldBe left
+        (2 * complex) shouldBe right
+    }
+
+    @Test
+    fun `should divide`() {
+        val complex = 1 + I
+        val left = (1 over 2) + (1 over 2).i // complex / two
+        val right = (1 over 2) + (1 over 2).i // two / complex
+
+        (complex / complex) shouldBe ONE
+        (complex / FloatingBigRational.TWO) shouldBe left
+        (FloatingBigRational.TWO / complex) shouldBe right
+        (complex / 2.0.big) shouldBe left
+        (2.0.big / complex) shouldBe right
+        (complex / 2.0) shouldBe left
+        (2.0 / complex) shouldBe right
+        (complex / 2.0f) shouldBe left
+        (2.0f / complex) shouldBe right
+        (complex / 2.big) shouldBe left
+        (2.big / complex) shouldBe right
+        (complex / 2L) shouldBe left
+        (2L / complex) shouldBe right
+        (complex / 2) shouldBe left
+        (2 / complex) shouldBe right
+    }
+
+    @Test
+    fun `should convert`() {
+        val one = ONE
+        one.toBigRational() shouldBe FloatingBigRational.ONE
+        one.toBigDecimal() shouldBe BFloating("1.0")
+        one.toDouble() shouldBe 1.0
+        one.toFloat() shouldBe 1.0f
+        one.toBigInteger() shouldBe BFixed.ONE
+        one.toLong() shouldBe 1L
+        one.toInt() shouldBe 1
+        (0 + 1.i).toImaginary() shouldBe 1.i
+        // Sad paths
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toBigRational() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toBigDecimal() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toDouble() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toFloat() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toBigInteger() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toLong() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toInt() }
+        shouldThrow<ArithmeticException> { ONE_PLUS_I.toImaginary() }
+    }
+
+    @Test
+    fun `should raise`() {
+        val half = 1 over 2
+        ONE_PLUS_I `^` 0 shouldBe 1 + 0.i
+        ONE_PLUS_I `^` 1 shouldBe ONE_PLUS_I
+        ONE_PLUS_I `^` -1 shouldBe half - half.i
+        ONE_PLUS_I `^` 2 shouldBe 0 + 2.i
+        ONE_PLUS_I `^` -2 shouldBe 0 - half.i
+    }
+
+    /** @todo Test all rules in [Math.pow] */
+    @Test
+    fun `should define by fiat 0^0`() {
+        ZERO `^` 0 shouldBe ONE
+    }
+
+    @Test
+    fun `should fail gracelessly on inverting zero`() {
+        // TODO As NaN is not comparable to itself, cleaner test?
+        val (real, imag) = ZERO `^` -1
+        real.isNaN().shouldBeTrue()
+        imag.value.isNaN().shouldBeTrue()
+    }
+
+    @Test
+    fun `should square root approximately`() {
+        val root = 1 + 2.i
+
+        (root * root).sqrtApproximated() shouldBe root
+    }
+}
